8364d1c5d274b262fbc3dbc33435cfe8a091ebf8
Michelle Austin <michelle.austin@itv.com>
2022-09-07 21:42:41 +0100

    fixing test

--- src/test/scala/CoordinateTranslatorTest.scala
+++ src/test/scala/CoordinateTranslatorTest.scala
@@ -14,9 +14,9 @@ class CoordinateTranslatorTest extends FunSuite {
   coordinateTranslatorTest(
     "Over and Up",
     List(Coordinates(0, 0), Coordinates(0, 1), Coordinates(1, 1)),
-    List(Clockwise(), Forward(), Anticlockwise(), Forward())
+    List(Forward(), Clockwise(), Forward())
   )
-  coordinateTranslatorTest("Up and Over", List(Coordinates(0, 0), Coordinates(1, 0), Coordinates(1, 1)), List(Forward(), Clockwise(), Forward()))
+  coordinateTranslatorTest("Up and Over", List(Coordinates(0, 0), Coordinates(1, 0), Coordinates(1, 1)), List(Clockwise(), Forward(), Anticlockwise(), Forward()))
   coordinateTranslatorTest(
     "A longer set of moves",
     List(Coordinates(0, 0), Coordinates(1, 0), Coordinates(1, 1), Coordinates(2, 1), Coordinates(3, 1), Coordinates(4, 1)),
