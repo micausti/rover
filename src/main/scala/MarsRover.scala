d919a8b5b02b71001482889e01efc4ba59ba2272
Michelle Austin <michelle.austin@itv.com>
2022-09-08 13:58:11 +0100

    tidying up

--- src/main/scala/MarsRover.scala
+++ src/main/scala/MarsRover.scala
@@ -5,8 +5,6 @@ object MarsRover extends IOApp.Simple {
 
   /*start with a grid, starting point, and destination
   calculate the shortest path from the starting point to the destination and produce a list of coordinates to represent the path
-  in the simplest case, the shortest path is the direct path between the start and end point
-  for more complex scenarios, there may be obstacles in the way; something like dijkstra's or a* algorithm could be used to determine the shortest path
   once the shortest path is known, that needs to be translated into a list of moves which tell the rover how to get from each position to the next
   the allowed moves are forward, clockwise, and anticlockwise
   the list of moves is printed to the console*/
@@ -16,7 +14,7 @@ object MarsRover extends IOApp.Simple {
   val destination: Coordinates = Coordinates(3, 3)
   def run: IO[Unit] =
     for {
-      _              <- IO.println("Calculating moves to destination")
+      _              <- IO.println(s"Calculating moves from starting point $start to destination $destination")
       translator     = new CoordinateTranslator
       allCoordinates = PathFinder.shortestPath(grid, start, destination)
       allMoves       <- allCoordinates.traverse(ls => translator.coordinatesToMoves(ls, IO(RunningListOfMoves(North, List.empty))).map(_.moves))
