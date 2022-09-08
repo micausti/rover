16a7bd2545028fb67869f0d04afddeca08d840c8
Michelle Austin <michelle.austin@itv.com>
2022-09-08 12:11:58 +0100

    Rework move (#3)
    * change how moves is modeled
    
    * cleaning up
    
    * formatting
    
    * update coordinate comparison
    
    * IO raise error to handle bad coordinates
    
    * Revert "IO raise error to handle bad coordinates"
    
    This reverts commit df5b3e7ac2c963406b9af33c130d9d29f460f908.

--- src/main/scala/Domain.scala
+++ src/main/scala/Domain.scala
@@ -17,7 +17,25 @@ object Coordinates {
   def keepOnGrid(currentValue: Int, gridMax: Int): Int =
     currentValue match {
       case valid if 0 <= currentValue && currentValue <= gridMax => valid
-      case l if currentValue < 0 => gridMax + currentValue
-      case h if currentValue > gridMax => currentValue - gridMax
+      case l if currentValue < 0                                 => gridMax + currentValue
+      case h if currentValue > gridMax                           => currentValue - gridMax
     }
+}
+
+sealed trait CoordinateComparison
+
+object CoordinateComparison {
+  case object XYMatch extends CoordinateComparison
+
+  case object IncrementXY extends CoordinateComparison
+
+  case object IncrementX extends CoordinateComparison
+
+  case object IncrementY extends CoordinateComparison
+
+  case object DecrementXY extends CoordinateComparison
+
+  case object DecrementX extends CoordinateComparison
+
+  case object DecrementY extends CoordinateComparison
 }
--- src/test/scala/PositionTest.scala
+++ src/test/scala/PositionTest.scala
@@ -9,7 +9,4 @@ class PositionTest extends FunSuite {
   keepOnGridCheck("value should be repositioned to the other side of the grid when value is negative", -1, 100, 99)
   keepOnGridCheck("value should be repositioned to the other side of the grid when the value is greater than grid max", 150, 100, 50)
   keepOnGridCheck("value should stay the same when it's in the bounds of the grid", 70, 100, 70)
-
-  val initialPosition: Coordinates  = Coordinates(10, 10)
-  val initialPosition2: Coordinates = Coordinates(10, 10)
 }
