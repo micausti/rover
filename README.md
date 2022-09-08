4dad4ca7f600f29509e86a2eb42c80db1cd25083
Michelle Austin <michelle.austin@itv.com>
2022-09-08 14:22:25 +0100

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+- start with a grid, starting point, and destination
+- calculate the shortest path from the starting point to the destination and produce a list of coordinates to represent the path
+- once the shortest path is known, that needs to be translated into a list of moves which tell the rover how to get from each position to the next 
+- the allowed moves are forward, clockwise, and anticlockwise
+- the list of moves is printed to the console
--- src/main/scala/MarsRover.scala
+++ src/main/scala/MarsRover.scala
@@ -3,12 +3,6 @@ import cats.implicits.toTraverseOps
 
 object MarsRover extends IOApp.Simple {
 
-  /*start with a grid, starting point, and destination
-  calculate the shortest path from the starting point to the destination and produce a list of coordinates to represent the path
-  once the shortest path is known, that needs to be translated into a list of moves which tell the rover how to get from each position to the next
-  the allowed moves are forward, clockwise, and anticlockwise
-  the list of moves is printed to the console*/
-
   val grid: Grid               = Grid(100, 100)
   val start: Coordinates       = Coordinates(0, 0)
   val destination: Coordinates = Coordinates(3, 3)
